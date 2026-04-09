b16f5dbcd6805e85ee459ac2f87d009976122ce6
Mustafiz <mustafizur.codes@gmail.com>
2026-04-10 02:15:08 +0600

    feat: add order status update view and enhance order creation logic

--- CampusKart/apps/orders/urls.py
+++ CampusKart/apps/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import OrderListCreateView, OrderDetailView
+from .views import OrderListCreateView, OrderDetailView, OrderStatusUpdateView
 
 app_name = "orders"
 
 urlpatterns = [
     path("",         OrderListCreateView.as_view(), name="order-list-create"),
     path("<int:pk>/",OrderDetailView.as_view(),     name="order-detail"),
+    path("<int:id>/status/", OrderStatusUpdateView.as_view(), name="order-status-update"),
 ]
